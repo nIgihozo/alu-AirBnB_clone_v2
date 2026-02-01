3b779d868533616314a30c70b905bff612e1117b
Nyiramanzi Igihozo <n.igihozo@alustudent.com>
2026-02-01 14:14:26 +0200

    Fixing error for DBStorage class for SQLAlchemy integration

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -67,18 +67,18 @@ class DBStorage:
     def delete(self, obj=None):
         """delete an element in the table
         """
-        if obj is not None:
-            self.__session.delete(obj)
+        if obj:
+            self.session.delete(obj)
 
     def reload(self):
         """configuration
         """
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session() # This creates the scoped session
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
+
     def close(self):
         """ calls remove()
         """
         self.__session.close()
-
