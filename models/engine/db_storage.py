f1088bf12d66406a76d53c92b086129486fe7102
Nyiramanzi Igihozo <n.igihozo@alustudent.com>
2026-02-01 15:12:56 +0200

    Pass DBStorage class for SQLAlchemy integration

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -91,11 +91,12 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sec)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         """ calls remove()
         """
         self.__session.close()
+
 
 
