439f2ef4d85805f060f377bb451de6c2aa6f64e4
Nyiramanzi Igihozo <n.igihozo@alustudent.com>
2026-02-01 15:28:34 +0200

    Updating storage initialization logic in __init__.py
    Refactor storage initialization to use a variable for storage type and improve module docstring.

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage or DBStorage"""
 import os
 
-if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
+storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
+if storage_type == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    # 1. We must import the models here so SQLAlchemy "Base" sees them
+    from models.state import State
+    from models.city import City
+    from models.user import User
+    from models.place import Place
+    from models.review import Review
+    from models.amenity import Amenity
+    # 2. NOW we can reload safely
     storage.reload()
 else:
-
     from models.engine.file_storage import FileStorage
-
     storage = FileStorage()
     storage.reload()
