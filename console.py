0e9f818cb410fe9cac21648bd0d0d8aa2c1272b7
Nyiramanzi Igihozo <n.igihozo@alustudent.com>
2026-02-01 16:04:25 +0200

    Remove duplicate import of Place in console.py
    Removed duplicate import of Place from models.

--- console.py
+++ console.py
@@ -5,11 +5,11 @@ from shlex import split
 from models import storage
 from datetime import datetime
 from models.base_model import BaseModel
+from models.place import Place
 from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
-from models.place import Place
 from models.review import Review
 
 
