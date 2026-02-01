7230deca7877ea1d186baa5736a9e7c1846adc45
Nyiramanzi Igihozo <n.igihozo@alustudent.com>
2026-02-01 15:56:26 +0200

    Refactor storage initialization logic
    Updated storage initialization to only import FileStorage when needed.

--- models/__init__.py
+++ models/__init__.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage or DBStorage"""
+"""This module instantiates an object of class FileStorage"""
 import os
 
-storage_type = os.getenv("HBNB_TYPE_STORAGE")
-
-if storage_type == "db":
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
+
     storage = DBStorage()
-    # 1. We must import the models here so SQLAlchemy "Base" sees them
-    from models.state import State
-    from models.city import City
-    from models.user import User
-    from models.place import Place
-    from models.review import Review
-    from models.amenity import Amenity
-    # 2. NOW we can reload safely
     storage.reload()
 else:
+
     from models.engine.file_storage import FileStorage
+
     storage = FileStorage()
     storage.reload()
